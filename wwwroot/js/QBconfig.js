4ad18575ea77f18595a01b64f96572e06129bbd4
rajnarayangupta <rajkumar00999.rk@gmail.com>
2024-07-13 00:22:31 +0530

    Create QBconfig.js

---
+++ wwwroot/js/QBconfig.js
@@ -0,0 +1,48 @@
+var QBconfig = {
+    credentials: {
+        'appId': -0,
+        'authKey': 'authkey',
+        'authSecret': 'authsecret',
+        'accountKey': 'acccountkey'
+    },
+    appConfig: {
+        chatProtocol: {
+            active: 2
+        },
+        endpoints: {
+            api: 'api.quickblox.com',
+            chat: 'chat.quickblox.com'
+        },
+        streamManagement: {
+            enable: true
+        },
+        debug: true,
+        callBackInterval: 30,
+    },
+};
+
+var appConfig = {
+    dilogsPerRequers: 15,
+    messagesPerRequest: 50,
+    usersPerRequest: 15,
+    typingTimeout: 3, // 3 seconds
+};
+
+var CONSTANTS = {
+    DIALOG_TYPES: {
+        CHAT: 3,
+        GROUPCHAT: 2,
+        PUBLICCHAT: 1
+    },
+    ATTACHMENT: {
+        TYPE: 'image',
+        BODY: '[attachment]',
+        MAXSIZE: 100000000, // set 100 megabytes,
+        MAXSIZEMESSAGE: 'The uploaded file exceeds maximum file size (100MB).'
+    },
+    NOTIFICATION_TYPES: {
+        NEW_DIALOG: '1',
+        UPDATE_DIALOG: '2',
+        LEAVE_DIALOG: '3'
+    }
+};
